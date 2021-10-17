fc0d90fdcf7b60317490d39a0a42e44a3f16dd4a
Bastien Lecoeur <lecoeur.bastien@gmail.com>
2021-10-17 23:05:23 +0100

    Picire support

--- scripts/automate_reducer.py
+++ scripts/automate_reducer.py
@@ -114,7 +114,7 @@ def run_reduction(reducer, compilers, exec_dirs, test_input, test_output, shader
                 f.close()
         else:
             print("Reduction failed for shader")
-            common.clean_files(os.getcwd(), ["test_reduced" + shader_tool.file_extension])
+            #common.clean_files(os.getcwd(), ["test_reduced" + shader_tool.file_extension])
     elif error_code >= 2000:
         print("Skipping test-case reduction for timeout shader")
         common.clean_files(os.getcwd(), ["test_reduced" + shader_tool.file_extension])
@@ -123,12 +123,12 @@ def run_reduction(reducer, compilers, exec_dirs, test_input, test_output, shader
         common.clean_files(os.getcwd(), ["test_reduced" + shader_tool.file_extension])
 
     # Cleans the current repository
-    common.clean_files(os.getcwd(),
-                       ["temp" + shader_tool.file_extension, reducer.input_file, reducer.output_files, reducer.interesting_test])
+    #common.clean_files(os.getcwd(),
+    #                   ["temp" + shader_tool.file_extension, reducer.input_file, reducer.output_files, reducer.interesting_test])
     residues = common.find_test_file(os.getcwd())
     if test_output in residues:
         residues.remove(test_output)
-    common.clean_files(os.getcwd(), residues)
+    #common.clean_files(os.getcwd(), residues)
 
 
 if __name__ == '__main__':
