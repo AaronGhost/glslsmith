8f92913c7fbfdcf151302497912254b323b56f25
Bastien Lecoeur <lecoeur.bastien@gmail.com>
2022-01-27 23:44:37 +0000

    Rewrote stat_shader and added shaders / buffer test base

--- scripts/stats_buffer.py
+++ scripts/stats_buffer.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 import argparse
 import os
 
@@ -20,7 +19,7 @@ import common
 
 
 def report_line_nb(filename):
-    line_count = 1
+    line_count = 0
     if os.path.isfile(filename):
         with open(filename, "r") as file:
             for line in file:
@@ -29,93 +28,89 @@ def report_line_nb(filename):
     return "missing file"
 
 
-def get_compiler_name_from_buffer(buffer_name):
+def get_compiler_name(buffer_name):
     return buffer_name.split("/")[-1].rsplit("_", 1)[0]
 
 
-def main():
-    parser = argparse.ArgumentParser(description="Print stats and info about difference showing buffers")
-    parser.add_argument('--report-seed', dest='compilers', default=[], nargs="+",
-                        help="Provide the compilers for which the seed number and the shader length will be reported, "
-                             "pass all to get the values for all compilers, and more_than_two for non-trivial case")
-    parser.add_argument('--verbose', dest="verbose", action="store_true", help="Gives the detail of agreeing compiler "
-                                                                               "for non-trivial case")
+def get_seed(buffer_name):
+    return buffer_name.split("/")[-1].rsplit("_", 1)[1].split(".")[0]
 
-    ns, exec_dirs, compilers_dict, _, shader_tool = common.env_setup(parser)
 
-    # Store names for the difference calculation
+def extract_seed_from_buffer_files(files):
+    res = []
+    cp_name = get_compiler_name(files[0])
+    for file in files:
+        if get_compiler_name(file) == cp_name:
+            res.append(get_seed(file))
+    return res
+
+
+def attribute_compiler_results(results, compilers_dict):
+    # A single compiler disagrees with the others (can be attributed to one compiler)
+    if len(results) == 2:
+        if len(results[0]) == 1 or len(results[1]) == 1:
+            if len(results[0]) == 1:
+                return get_compiler_name(results[0][0])
+            else:
+                return get_compiler_name(results[1][0])
+        # A family of compilers disagrees (Angle)
+        if (all(compilers_dict[get_compiler_name(buffer_name)].type == "angle"
+                for buffer_name in results[0])
+            and all(compilers_dict[get_compiler_name(buffer_name)].type == "independent"
+                    for buffer_name in results[1])) \
+                or (all(compilers_dict[get_compiler_name(buffer_name)].type == "angle"
+                        for buffer_name in results[1])
+                    and all(compilers_dict[get_compiler_name(buffer_name)].type == "independent"
+                            for buffer_name in results[0])):
+            return "angle"
+    # Something else is happening
+    return "more than two"
+
+
+def stats_buffers(buffer_dir, shader_dir, compilers_dict, shader_tools, verbose):
     compiler_differences = {}
     for compiler_name in compilers_dict:
         compiler_differences[compiler_name] = 0
     compiler_differences["angle"] = 0
-    compiler_differences["more_than_two"] = 0
+    compiler_differences["more than two"] = 0
     # Get a list of the files in the directory
-    file_list = os.listdir(exec_dirs.keptbufferdir)
-    seeds = []
-    any_name = next(iter(compilers_dict))
-    for file in file_list:
-        termination = file.split("_")[-1]
-        compiler_radix = file.replace("_" + termination, "")
-        if any_name == compiler_radix:
-            seeds.append(termination.split(".")[0])
+    seeds = extract_seed_from_buffer_files(os.listdir(buffer_dir))
 
     print(str(len(seeds)) + " different seeds")
     for seed in seeds:
         correct_seed_buffers = []
         for compiler_name in compilers_dict:
-            correct_seed_buffers.append(exec_dirs.keptbufferdir + compiler_name + "_" + seed + ".txt")
+            correct_seed_buffers.append(buffer_dir + compiler_name + "_" + seed + ".txt")
         results = common.comparison_helper(correct_seed_buffers)
-        # Read back results from the comparison
-        if len(results) == 2:
-            if len(results[0]) == 1 or len(results[1]) == 1:
-                if len(results[0]) == 1:
-                    compiler_name = get_compiler_name_from_buffer(results[0][0])
-                else:
-                    compiler_name = get_compiler_name_from_buffer(results[1][0])
-                if compiler_name in ns.compilers or "all" in ns.compilers:
-                    print(compiler_name + ", lines: " + report_line_nb(
-                        exec_dirs.keptshaderdir + seed + ".shadertrap") + ", seed: " + seed)
-                compiler_differences[compiler_name] += 1
-                continue
+        # Read back results from the comparison and attribute the defect to a group
+        group_name = attribute_compiler_results(results, compilers_dict)
+        compiler_differences[group_name] += 1
 
-            # Try if we are in the angle case
-            if (all(compilers_dict[get_compiler_name_from_buffer(buffer_name)].type == "angle"
-                    for buffer_name in results[0])
-                and all(compilers_dict[get_compiler_name_from_buffer(buffer_name)].type == "independent"
-                        for buffer_name in results[1])) \
-                    or (all(compilers_dict[get_compiler_name_from_buffer(buffer_name)].type == "angle"
-                            for buffer_name in results[1])
-                        and all(compilers_dict[get_compiler_name_from_buffer(buffer_name)].type == "independent"
-                                for buffer_name in results[0])):
-                if "angle" in ns.compilers or "all" in ns.compilers:
-                    print("angle" + ", lines: " + report_line_nb(
-                        exec_dirs.keptshaderdir + seed + ".shadertrap") + ", seed: " + seed)
-                compiler_differences["angle"] += 1
-                continue
+        if verbose and group_name == "more than two":
+            print(list(map(lambda x: list(map(lambda y: get_compiler_name(y), x)), results)))
+        # Report the number of lines for each shader
+        for tool in shader_tools:
+            if os.path.isfile(shader_dir + seed + tool.file_extension):
 
-        # Everything else where a cause is difficult to identify
-        compiler_differences["more_than_two"] += 1
-        if "more_than_two" in ns.compilers or "all" in ns.compilers:
-            print("More than two different values, lines: " + report_line_nb(
-                exec_dirs.keptshaderdir + seed + ".shadertrap")
-                  + ", seed: " + seed)
-        if ns.verbose:
-            if len(results) == len(compilers_dict):
-                print("all compilers disagree\n")
-            else:
-                compilers_text = ""
-                for agreeing_compilers in results:
-                    for buffer_name in agreeing_compilers:
-                        compilers_text += get_compiler_name_from_buffer(buffer_name) + ", "
-                    compilers_text = compilers_text[:-2]
-                    compilers_text += " compilers agree, "
-                compilers_text = compilers_text[:-2]
-                print(compilers_text + "\n")
+                print("Group: " + group_name + ", lines: " + report_line_nb(
+                    shader_dir + seed + tool.file_extension) + ", seed: " + seed)
+                break
 
+    # Print a summary
+    print("========= SUMMARY ================================================================")
     for compiler_name in compilers_dict:
         print(compiler_name + " different values: " + str(compiler_differences[compiler_name]))
     print("angle different values: " + str(compiler_differences["angle"]))
-    print("more than two groups of values: " + str(compiler_differences["more_than_two"]))
+    print("more than two groups of values: " + str(compiler_differences["more than two"]))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Print stats and info about difference showing buffers")
+    parser.add_argument('--verbose', dest="verbose", action="store_true", help="Gives the detail of agreeing compiler "
+                                                                               "for non-trivial case")
+
+    ns, exec_dirs, compilers_dict, _, shader_tools = common.env_setup(parser)
+    stats_buffers(exec_dirs.keptbufferdir, exec_dirs.keptshaderdir, compilers_dict, shader_tools, ns.verbose)
 
 
 if __name__ == "__main__":
--- scripts/test/test_stats_buffer.py
+++ scripts/test/test_stats_buffer.py
@@ -11,12 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import pytest
 
-from scripts.stats_buffer import report_line_nb, get_compiler_name_from_buffer
+from scripts.common import Compiler, ShaderTool
+from scripts.stats_buffer import report_line_nb, get_compiler_name, extract_seed_from_buffer_files, get_seed, \
+    attribute_compiler_results, stats_buffers
 
 
-@pytest.mark.parametrize("filename, result", [("shader_1.shadertrap", "44"), ("shader_2.shadertrap", "36"),
+@pytest.mark.parametrize("filename, result", [("shader_1.shadertrap", "43"), ("shader_2.shadertrap", "35"),
                                               ("missing.shadertrap", "missing file")])
 def test_report_line_nb(filename, result):
     assert report_line_nb("testdata/stats_buffer/" + filename) == result
@@ -24,5 +27,86 @@ def test_report_line_nb(filename, result):
 
 @pytest.mark.parametrize("buffer_name, compiler_name",
                          [("whatever/nvidia_0.txt", "nvidia"), ("vulkan_intel_522.txt", "vulkan_intel")])
-def test_get_compiler_name_from_buffer(buffer_name, compiler_name):
-    assert get_compiler_name_from_buffer(buffer_name) == compiler_name
+def test_get_compiler_name(buffer_name, compiler_name):
+    assert get_compiler_name(buffer_name) == compiler_name
+
+
+@pytest.mark.parametrize("buffer_name, seed",
+                         [("whatever/nvidia_0.txt", "0"), ("vulkan_intel_522.txt", "522")])
+def test_get_compiler_name(buffer_name, seed):
+    assert get_seed(buffer_name) == seed
+
+
+@pytest.mark.parametrize("file_list, unique_seed",
+                         [(["a_0.txt", "a_1.txt", "a_2.txt", "b_0.txt", "b_1.txt", "b_2.txt"], ["0", "1", "2"]),
+                          (["a_x_0.txt", "b_0.txt", "a_x_1.txt", "b_x_1.txt", "a_0.txt", "a_1.txt", "a_150.txt",
+                            "a_x_150.txt", "a_x_444.txt"]
+                           , ["0", "1", "150", "444"])])
+def test_extract_seed_from_buffer_files(file_list, unique_seed):
+    assert extract_seed_from_buffer_files(file_list) == unique_seed
+
+
+@pytest.mark.parametrize("results, group", [([["a", "b"], ["c"]], "c"),
+                                            ([["a"], ["b"]], "a"),
+                                            ([["a", "c"], ["b", "d"]], "angle"),
+                                            ([["b", "d", "e"], ["a", "f"]], "angle"),
+                                            ([["a", "b"], ["c", "d"]], "more than two"),
+                                            ([["a"], ["b"], ["c"], ["d"]], "more than two")])
+def test_attribute_compiler_results(results, group):
+    compiler_dict = {"a": Compiler("a", "a", "angle", "", "", []),
+                     "b": Compiler("b", "b", "independent", "", "", []),
+                     "c": Compiler("c", "c", "angle", "", "", []),
+                     "d": Compiler("d", "d", "independent", "", "", []),
+                     "e": Compiler("e", "e", "independent", "", "", []),
+                     "f": Compiler("f", "f", "angle", "", "", [])}
+    assert attribute_compiler_results(results, compiler_dict) == group
+
+
+def test_stats_buffers(capsys):
+    # Prepare compilers for the scenario
+    compiler_dict = {"a": Compiler("a", "a", "angle", "", "", []),
+                     "b": Compiler("b", "b", "independent", "", "", []),
+                     "c": Compiler("c", "c", "angle", "", "", []),
+                     "d": Compiler("d", "d", "independent", "", "", []),
+                     "a_x": Compiler("a_x", "a_x", "angle", "", "", [])}
+    shadertools = [ShaderTool("shadertrap", "shadertrap/shadertrap", ".shadertrap")]
+
+    # Lines that should be in the normal output
+    lines = ["11 different seeds\n",
+             "Group: a, lines: 12, seed: 0\n",
+             "Group: a, lines: 15, seed: 1\n",
+             "Group: angle, lines: 18, seed: 2\n",
+             "Group: more than two, lines: 21, seed: 3\n",
+             "Group: c, lines: 14, seed: 4\n",
+             "Group: angle, lines: 9, seed: 5\n",
+             "Group: d, lines: 17, seed: 15\n",
+             "Group: a, lines: 4, seed: 333\n",
+             "Group: more than two, lines: 5, seed: 522\n",
+             "Group: more than two, lines: 2, seed: 2358\n",
+             "Group: a_x, lines: 13, seed: 9999\n",
+             "========= SUMMARY ================================================================\n",
+             "a different values: 3\n",
+             "b different values: 0\n",
+             "c different values: 1\n",
+             "d different values: 1\n",
+             "a_x different values: 1\n",
+             "angle different values: 2\n",
+             "more than two groups of values: 3\n"]
+
+    # All lines from the normal output should be in, the more than two group each carry an extra line before hand
+    verbose_lines = lines.copy()
+    verbose_lines[4] = "[['a', 'a_x'], ['b', 'c'], ['d']]\nGroup: more than two, lines: 21, seed: 3\n"
+    verbose_lines[9] = "[['a'], ['b'], ['c'], ['d'], ['a_x']]\nGroup: more than two, lines: 5, seed: 522\n"
+    verbose_lines[10] = "[['a', 'b', 'a_x'], ['c', 'd']]\nGroup: more than two, lines: 2, seed: 2358\n"
+
+    # Test for the length of the outputs and then for all "lines" contained in it
+    stats_buffers("testdata/keptbuf/", "testdata/keptshad/", compiler_dict, shadertools, False)
+    outputs = capsys.readouterr()
+    assert len(outputs.out.splitlines()) == 20
+    for line in lines:
+        assert line in str(outputs.out)
+    stats_buffers("testdata/keptbuf/", "testdata/keptshad/", compiler_dict, shadertools, True)
+    outputs = capsys.readouterr()
+    assert len(outputs.out.splitlines()) == 23
+    for line in verbose_lines:
+        assert line in str(outputs.out)
